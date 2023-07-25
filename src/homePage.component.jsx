6362cd9ce02a7726d7c98692e76bc1fd7abbc9d7
Asmare Zelalem <asmarestu12@gmail.com>
2023-07-25 21:21:19 +0300

    create project with create-reat-app and add files to home page

---
+++ src/homePage.component.jsx
@@ -0,0 +1,41 @@
+/** @format */
+
+import React from "react";
+import "./homepage.style.scss";
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="sub-title">Shope Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jackets</h1>
+          <span className="sub-title">Shope Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Trousers</h1>
+          <span className="sub-title">Shope Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Men</h1>
+          <span className="sub-title">Shope Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Women</h1>
+          <span className="sub-title">Shope Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
